2655f677d6fba86ae88c245c3bf5855d7e105d58
SupItsMike <github@supitsmike.dev>
2025-09-23 07:26:28 -0400

    Create form layout.

--- DABMusicDownloader/Forms/MainForm.Designer.cs
+++ DABMusicDownloader/Forms/MainForm.Designer.cs
@@ -29,20 +29,150 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            statusStrip1 = new StatusStrip();
+            panel1 = new Panel();
+            lblSearchResults = new Label();
+            cmbSearchType = new ComboBox();
+            txtSearchQuery = new TextBox();
+            btnSearch = new Button();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            settingsToolStripMenuItem = new ToolStripMenuItem();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.SizingGrip = false;
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(lblSearchResults);
+            panel1.Controls.Add(cmbSearchType);
+            panel1.Controls.Add(txtSearchQuery);
+            panel1.Controls.Add(btnSearch);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 24);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 29);
+            panel1.TabIndex = 2;
+            // 
+            // lblSearchResults
+            // 
+            lblSearchResults.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblSearchResults.Location = new Point(500, 3);
+            lblSearchResults.Name = "lblSearchResults";
+            lblSearchResults.Size = new Size(297, 23);
+            lblSearchResults.TabIndex = 3;
+            lblSearchResults.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // cmbSearchType
+            // 
+            cmbSearchType.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbSearchType.FormattingEnabled = true;
+            cmbSearchType.Items.AddRange(new object[] { "Track", "Album", "Artist" });
+            cmbSearchType.Location = new Point(434, 3);
+            cmbSearchType.Name = "cmbSearchType";
+            cmbSearchType.Size = new Size(60, 23);
+            cmbSearchType.TabIndex = 2;
+            // 
+            // txtSearchQuery
+            // 
+            txtSearchQuery.Location = new Point(84, 3);
+            txtSearchQuery.Name = "txtSearchQuery";
+            txtSearchQuery.Size = new Size(344, 23);
+            txtSearchQuery.TabIndex = 1;
+            txtSearchQuery.KeyDown += txtSearchQuery_KeyDown;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(3, 3);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(75, 23);
+            btnSearch.TabIndex = 0;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, settingsToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            settingsToolStripMenuItem.Size = new Size(61, 20);
+            settingsToolStripMenuItem.Text = "Settings";
+            settingsToolStripMenuItem.Click += settingsToolStripMenuItem_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 53);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(800, 375);
+            dataGridView1.TabIndex = 4;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private StatusStrip statusStrip1;
+        private Panel panel1;
+        private ComboBox cmbSearchType;
+        private TextBox txtSearchQuery;
+        private Button btnSearch;
+        private Label lblSearchResults;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private DataGridView dataGridView1;
     }
 }
--- DABMusicDownloader/Forms/MainForm.cs
+++ DABMusicDownloader/Forms/MainForm.cs
@@ -5,6 +5,23 @@ namespace DABMusicDownloader.Forms
         public MainForm()
         {
             InitializeComponent();
+
+            cmbSearchType.SelectedIndex = 0;
+        }
+
+        private void txtSearchQuery_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+
+            btnSearch.PerformClick();
+
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new SettingsForm().ShowDialog(this);
         }
     }
 }
