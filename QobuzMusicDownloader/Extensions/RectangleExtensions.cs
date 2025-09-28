f9a3b77218cfea98fb5726b215386368dc46eada
SupItsMike <github@supitsmike.dev>
2025-09-28 19:34:41 -0400

    Fix error when radius is 0 in `GetRoundedRectanglePath`.

--- QobuzMusicDownloader/Extensions/RectangleExtensions.cs
+++ QobuzMusicDownloader/Extensions/RectangleExtensions.cs
@@ -7,19 +7,27 @@ namespace QobuzMusicDownloader.Extensions
         public static GraphicsPath GetRoundedRectanglePath(this Rectangle rect, int radius)
         {
             var path = new GraphicsPath();
-            var diameter = radius * 2;
 
-            // Top-left corner
-            path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+            if (radius == 0)
+            {
+                path.AddRectangle(rect);
+            }
+            else
+            {
+                var diameter = radius * 2;
 
-            // Top-right corner
-            path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+                // Top-left corner
+                path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
 
-            // Bottom-right corner
-            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+                // Top-right corner
+                path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
 
-            // Bottom-left corner
-            path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+                // Bottom-right corner
+                path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+
+                // Bottom-left corner
+                path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            }
 
             path.CloseFigure();
             return path;
