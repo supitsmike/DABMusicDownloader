fbe4ebeacc3f62d4cc4235f079f7912e8f68bedc
SupItsMike <github@supitsmike.dev>
2025-09-29 00:08:49 -0400

    Add new search form with the new look.

---
+++ QobuzMusicDownloader/Forms/SearchForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿using QobuzMusicDownloader.CustomControls;
+
+namespace QobuzMusicDownloader.Forms
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flpSearchResults = new FlowLayoutPanel();
+            btnSearch = new Button();
+            txtSearchQuery = new TextBox();
+            cmbSearchType = new ComboBox();
+            btnSettings = new Button();
+            SuspendLayout();
+            // 
+            // flpSearchResults
+            // 
+            flpSearchResults.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            flpSearchResults.AutoScroll = true;
+            flpSearchResults.Location = new Point(0, 48);
+            flpSearchResults.Name = "flpSearchResults";
+            flpSearchResults.Size = new Size(899, 513);
+            flpSearchResults.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnSearch.Font = new Font("Segoe UI", 12.5F);
+            btnSearch.ForeColor = SystemColors.ControlText;
+            btnSearch.Location = new Point(673, 12);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(75, 30);
+            btnSearch.TabIndex = 1;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // txtSearchQuery
+            // 
+            txtSearchQuery.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtSearchQuery.Font = new Font("Segoe UI", 12.5F);
+            txtSearchQuery.Location = new Point(238, 12);
+            txtSearchQuery.Name = "txtSearchQuery";
+            txtSearchQuery.PlaceholderText = "Search for anything...";
+            txtSearchQuery.Size = new Size(429, 30);
+            txtSearchQuery.TabIndex = 2;
+            txtSearchQuery.TextChanged += txtSearchQuery_TextChanged;
+            // 
+            // cmbSearchType
+            // 
+            cmbSearchType.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbSearchType.Font = new Font("Segoe UI", 12F);
+            cmbSearchType.FormattingEnabled = true;
+            cmbSearchType.Items.AddRange(new object[] { "Albums", "Tracks", "Atrists" });
+            cmbSearchType.Location = new Point(152, 12);
+            cmbSearchType.Name = "cmbSearchType";
+            cmbSearchType.Size = new Size(80, 29);
+            cmbSearchType.TabIndex = 3;
+            // 
+            // btnSettings
+            // 
+            btnSettings.ForeColor = SystemColors.ControlText;
+            btnSettings.Location = new Point(12, 12);
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(23, 23);
+            btnSettings.TabIndex = 4;
+            btnSettings.Text = "Settings";
+            btnSettings.UseVisualStyleBackColor = true;
+            btnSettings.Click += btnSettings_Click;
+            // 
+            // SearchForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(899, 561);
+            Controls.Add(btnSettings);
+            Controls.Add(cmbSearchType);
+            Controls.Add(txtSearchQuery);
+            Controls.Add(btnSearch);
+            Controls.Add(flpSearchResults);
+            Name = "SearchForm";
+            Text = "Search Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flpSearchResults;
+        private Button btnSearch;
+        private TextBox txtSearchQuery;
+        private ComboBox cmbSearchType;
+        private Button btnSettings;
+    }
+}
---
+++ QobuzMusicDownloader/Forms/SearchForm.cs
@@ -0,0 +1,89 @@
+﻿using QobuzMusicDownloader.QobuzDL;
+using QobuzMusicDownloader.QobuzDL.Album;
+using QobuzMusicDownloader.QobuzDL.Core;
+using QobuzMusicDownloader.UserControls;
+
+namespace QobuzMusicDownloader.Forms
+{
+    public partial class SearchForm : Form
+    {
+        private string _currentSearchQuery = string.Empty;
+        private SearchFilter _currentSearchType = SearchFilter.Albums;
+        private int _currentSearchOffset;
+
+        public SearchForm()
+        {
+            InitializeComponent();
+
+            cmbSearchType.SelectedIndex = 0;
+            txtSearchQuery.Text = @"Kanye West";
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            _currentSearchQuery = txtSearchQuery.Text;
+            _currentSearchType = (SearchFilter)cmbSearchType.SelectedIndex;
+            _currentSearchOffset = 0;
+
+            await SearchQobuzDL();
+        }
+
+        private async Task SearchQobuzDL()
+        {
+            var response = await QobuzDLAPI.GetMusicAsync(_currentSearchQuery, _currentSearchOffset);
+            if (response == null || response.Data == null) return;
+
+            flpSearchResults.Controls.Clear();
+            if (_currentSearchType == SearchFilter.Albums)
+            {
+                foreach (var album in response.Data.Albums.Items)
+                {
+                    if (album == null) continue;
+
+                    var albumCard = new AlbumCard(album);
+                    //albumCard.AlbumClicked += (s, e) => OnAlbumClicked(album);
+                    //albumCard.AlbumDoubleClicked += (s, e) => OnAlbumDoubleClick(album);
+
+                    flpSearchResults.Controls.Add(albumCard);
+                }
+            }
+        }
+
+        //private void OnAlbumClicked(QobuzAlbum album)
+        //{
+        //    MessageBox.Show($@"Clicked: {album.Title} by {album.Artist}");
+        //}
+        //
+        //private void OnAlbumDoubleClick(QobuzAlbum album)
+        //{
+        //    MessageBox.Show($@"Double Clicked: {album.Title} by {album.Artist}");
+        //}
+
+        private void txtSearchQuery_TextChanged(object sender, EventArgs e)
+        {
+            btnSearch.Enabled = string.IsNullOrWhiteSpace(txtSearchQuery.Text) == false;
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            new SettingsForm().ShowDialog(this);
+            Invalidate();
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (Properties.Settings.Default.DarkMode)
+            {
+                BackColor = Color.FromArgb(20, 20, 20);
+                ForeColor = SystemColors.Control;
+            }
+            else
+            {
+                BackColor = SystemColors.Control;
+                ForeColor = SystemColors.ControlText;
+            }
+
+            base.OnPaint(e);
+        }
+    }
+}
