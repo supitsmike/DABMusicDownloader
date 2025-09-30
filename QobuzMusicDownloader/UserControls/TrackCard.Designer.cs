656cb7b33370b0b3a1dea1b26dcc8a1aabdcf23a
SupItsMike <github@supitsmike.dev>
2025-09-29 20:56:08 -0400

    Add TrackCard.

---
+++ QobuzMusicDownloader/UserControls/TrackCard.Designer.cs
@@ -0,0 +1,114 @@
+﻿using QobuzMusicDownloader.CustomControls;
+
+namespace QobuzMusicDownloader.UserControls
+{
+    partial class TrackCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTrackTitle = new ScrollableLabel();
+            lblArtistName = new ScrollableLabel();
+            lblExplicit = new Label();
+            lblAlbumTitle = new ScrollableLabel();
+            SuspendLayout();
+            // 
+            // lblTrackTitle
+            // 
+            lblTrackTitle.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblTrackTitle.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblTrackTitle.ForeColor = SystemColors.ControlText;
+            lblTrackTitle.Location = new Point(21, 203);
+            lblTrackTitle.Name = "lblTrackTitle";
+            lblTrackTitle.Size = new Size(179, 15);
+            lblTrackTitle.TabIndex = 1;
+            lblTrackTitle.Text = "Track Title";
+            lblTrackTitle.TextAlign = ContentAlignment.MiddleLeft;
+            lblTrackTitle.UseMnemonic = false;
+            lblTrackTitle.Click += TrackCard_Click;
+            lblTrackTitle.DoubleClick += TrackCard_DoubleClick;
+            // 
+            // lblArtistName
+            // 
+            lblArtistName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblArtistName.ForeColor = SystemColors.GrayText;
+            lblArtistName.Location = new Point(0, 221);
+            lblArtistName.Name = "lblArtistName";
+            lblArtistName.Size = new Size(200, 15);
+            lblArtistName.TabIndex = 2;
+            lblArtistName.Text = "Artist Name";
+            lblArtistName.TextAlign = ContentAlignment.MiddleLeft;
+            lblArtistName.UseMnemonic = false;
+            lblArtistName.Click += TrackCard_Click;
+            lblArtistName.DoubleClick += TrackCard_DoubleClick;
+            // 
+            // lblExplicit
+            // 
+            lblExplicit.BackColor = SystemColors.ControlText;
+            lblExplicit.Font = new Font("Segoe UI", 7F, FontStyle.Bold);
+            lblExplicit.ForeColor = SystemColors.Control;
+            lblExplicit.Location = new Point(0, 203);
+            lblExplicit.Name = "lblExplicit";
+            lblExplicit.Size = new Size(15, 15);
+            lblExplicit.TabIndex = 4;
+            lblExplicit.Text = "E";
+            lblExplicit.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblAlbumTitle
+            // 
+            lblAlbumTitle.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblAlbumTitle.ForeColor = SystemColors.GrayText;
+            lblAlbumTitle.Location = new Point(0, 239);
+            lblAlbumTitle.Name = "lblAlbumTitle";
+            lblAlbumTitle.Size = new Size(200, 15);
+            lblAlbumTitle.TabIndex = 5;
+            lblAlbumTitle.Text = "Album Title";
+            lblAlbumTitle.TextAlign = ContentAlignment.MiddleLeft;
+            lblAlbumTitle.UseMnemonic = false;
+            // 
+            // TrackCard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(lblAlbumTitle);
+            Controls.Add(lblExplicit);
+            Controls.Add(lblTrackTitle);
+            Controls.Add(lblArtistName);
+            Margin = new Padding(10);
+            Name = "TrackCard";
+            Size = new Size(200, 255);
+            Click += TrackCard_Click;
+            DoubleClick += TrackCard_DoubleClick;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private ScrollableLabel lblTrackTitle;
+        private ScrollableLabel lblArtistName;
+        private Label lblExplicit;
+        private ScrollableLabel lblAlbumTitle;
+    }
+}
---
+++ QobuzMusicDownloader/UserControls/TrackCard.cs
@@ -0,0 +1,129 @@
+﻿using QobuzMusicDownloader.Extensions;
+using QobuzMusicDownloader.QobuzDL.Album;
+using QobuzMusicDownloader.QobuzDL.Track;
+using QobuzMusicDownloader.Services;
+using System.Drawing.Drawing2D;
+
+namespace QobuzMusicDownloader.UserControls
+{
+    public partial class TrackCard : UserControl
+    {
+        private readonly IImageCacheService _imageCacheService;
+        private readonly QobuzTrack? _track;
+
+        private Image? _albumCover;
+
+        public TrackCard(QobuzTrack? track)
+        {
+            _track = track;
+            _imageCacheService = ServiceLocator.GetService<IImageCacheService>();
+            InitializeComponent();
+
+            UpdateAlbumDisplay();
+        }
+
+        private void UpdateAlbumInfo()
+        {
+            lblTrackTitle.Text = _track?.Title;
+            lblArtistName.Text = _track?.Album.Artist.Name;
+            lblAlbumTitle.Text = _track?.Album.Title;
+
+            if (_track?.ParentalWarning == false)
+            {
+                lblExplicit.Visible = false;
+                lblTrackTitle.Location = new Point(0, lblTrackTitle.Location.Y);
+                lblTrackTitle.Size = new Size(200, lblTrackTitle.Size.Height);
+            }
+        }
+
+        private async void UpdateAlbumDisplay()
+        {
+            try
+            {
+                if (_track == null)
+                {
+                    lblTrackTitle.Visible = false;
+                    lblArtistName.Visible = false;
+                    lblAlbumTitle.Visible = false;
+                    lblExplicit.Visible = false;
+                    return;
+                }
+
+                UpdateAlbumInfo();
+
+                _albumCover = await _imageCacheService.GetImageAsync(_track.Album.Image.Small, _track.Album.Id);
+
+                Invalidate();
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+        }
+
+        public event EventHandler<QobuzTrack?>? TrackClicked;
+        private void TrackCard_Click(object sender, EventArgs e)
+        {
+            TrackClicked?.Invoke(this, _track);
+        }
+
+        public event EventHandler<QobuzTrack?>? TrackDoubleClicked;
+        private void TrackCard_DoubleClick(object sender, EventArgs e)
+        {
+            TrackDoubleClicked?.Invoke(this, _track);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (Properties.Settings.Default.DarkMode)
+            {
+                lblExplicit.BackColor = SystemColors.Control;
+                lblExplicit.ForeColor = SystemColors.ControlText;
+
+                lblTrackTitle.ForeColor = SystemColors.Control;
+                lblArtistName.ForeColor = SystemColors.ControlDark;
+                lblAlbumTitle.ForeColor = SystemColors.ControlDark;
+            }
+            else
+            {
+                lblExplicit.BackColor = SystemColors.ControlText;
+                lblExplicit.ForeColor = SystemColors.Control;
+
+                lblTrackTitle.ForeColor = SystemColors.ControlText;
+                lblArtistName.ForeColor = SystemColors.GrayText;
+                lblAlbumTitle.ForeColor = SystemColors.GrayText;
+            }
+
+            var graphics = e.Graphics;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            var state = graphics.Save();
+            var albumCoverRect = new Rectangle(0, 0, 200, 200);
+            using var path = albumCoverRect.GetRoundedRectanglePath(12);
+
+            if (_albumCover != null)
+            {
+                graphics.SetClip(path);
+                graphics.DrawImage(_albumCover, albumCoverRect);
+            }
+            else
+            {
+                using var brush = new SolidBrush(SystemColors.WindowFrame);
+                graphics.FillPath(brush, path);
+            }
+
+            graphics.Restore(state);
+
+            base.OnPaint(e);
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            Invalidate();
+        }
+    }
+}
